4d9763e3c2b296eded3ee2ad093380505ad5f43a
unknown <guddyd6761@naver.com>
2023-01-05 16:38:58 +0900

    vue기초편

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{propsdata}}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['propsdata'],
+    methods: {
+      sendEvent:function(){
+        this.$emit('renew')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
